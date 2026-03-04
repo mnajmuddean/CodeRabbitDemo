eeff7f9ac966b15705469d8e1e7781c2cb6d945b
CORPORATE\najmuddin.amdan <najmuddin.amdan@posdigicert.com.my>
2026-03-04 16:37:53 +0800

    trigger PR for review

--- CodeRabbitDemo/Program.cs
+++ CodeRabbitDemo/Program.cs
@@ -1,4 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using CodeRabbitDemo.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Database context
+builder.Services.AddDbContext<AppDbContext>(options =>
+ options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 
